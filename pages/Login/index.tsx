2ed104ba1078635ed50b8b2d4471e6905988c908
Lucas Matos <matoslucas.lm@gmail.com>
2024-01-23 09:11:46 -0300

    inicio pagina de login

---
+++ pages/Login/index.tsx
@@ -0,0 +1,5 @@
+import React, { useState, useContext } from "react";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+// import { LinearGradient } from "expo-linear-gradient";
+// import TextInputWithLabel from "../../components/TextInputWithLabel";
+import styles from "./styles";
---
+++ pages/Login/styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+
+
+
+})
+
+export default styles
