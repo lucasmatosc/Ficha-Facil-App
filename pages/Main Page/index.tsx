4a5d4144096271d82407a5572fcb8922c4d9db38
lucasmatosc <matoslucas.lm@gmail.com>
2024-01-24 00:20:17 -0300

    Início estilização pagina principal

---
+++ pages/Main Page/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import styles from "./styles"
+
+export default function MainPage(){
+
+
+
+
+
+
+}
